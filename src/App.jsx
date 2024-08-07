60b1be93a9415a2ec57b63ae51d7b93379768d0d
YANGSEOKWOO <didtjrdn030@naver.com>
2024-08-07 18:22:27 +0900

    css : 모바일, 웹 반응형 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import { Map } from "react-kakao-maps-sdk";
 import Marker from "./components/atoms/Marker";
+import ResponsiveLayout from "./components/Layout/ResponsiveLayout";
 
 function App() {
   // TODO :: center위도 경도, 사용자의 위치를 가져와서 처음 위치를 볼 수 있도록 변환
   // TODO :: 추후 Data Fetch 할 때, Marker는 계속 가져와야하도록 수정
   return (
-      <Map
-        center={{ lat: 33.450701, lng: 126.570667 }}
-        style={{ width: "100%", height: "100%" }}
-      >
-        <Marker lat={33.450701} lng={126.570667} isCleaned={true} id={1}/>
-      {/* 필요한 만큼 Marker 컴포넌트를 추가 */}
-        <Marker lat={33.451701} lng={126.571667} isCleaned={false} id = {2} />
-      </Map>
+    <ResponsiveLayout/>
   );
 }
 
---
+++ src/components/Layout/MobileLayout.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+// import MobileSideBar from 'components/Sidebar/MobileSidebar';
+// import MobileNavbar from 'components/Navbar/MobileNavbar';
+// import COLOR from 'constants/color.constant';
+
+const MobileLayout = (props) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "blue",
+        width: '100%',
+      }}
+    >
+        모바일
+      {/* <MobileNavbar /> */}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          maxWidth: '1170px',
+          margin: 'auto',
+          marginTop: '4rem',
+          height: '100%',
+        }}
+      >
+        {/* <MobileSideBar /> */}
+        <div>{props.children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileLayout;
