4d928b71a0c31cc10d4a3fea63a72bae9f21b77c
YANGSEOKWOO <didtjrdn030@naver.com>
2024-08-11 22:45:35 +0900

    feat: api연결 instance 작성

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,10 +1,34 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const instance = axios.create({
-    baseURL: import.meta.env.VITE_APP_BASE_URL,
-    headers:{
-        "Content-Type": 'application.json'
-    }
-});
+  baseURL: import.meta.env.VITE_APP_BASE_URL,
+  headers: {
+    'Content-Type': 'application.json',
+  },
+})
 
+/**
+ * Ballon들의 List를 가져오는 함수
+ *
+ * @returns {object} ballons
+ */
+export const getBallonListData = () => {
+  //   return instance.get('/ballons')
+}
 
+// const mockupBallonListData ={
+//     "success": bool,
+// 	"data": [
+// 		{
+// 			"id": int,
+// 			"address": str,
+// 			"detection_image": str,
+// 			"detection_time": datetime,
+// 			"processing_image": str,
+// 			"processing_time": datetime,
+// 			"processing_state": enum,
+// 			"description": str
+// 		},
+// 	],
+// 	"error": None
+// }
