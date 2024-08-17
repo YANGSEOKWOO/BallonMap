1f212f8e333655894a217d8fec2dd84f725c6c4e
YANGSEOKWOO <didtjrdn030@naver.com>
2024-08-17 18:45:26 +0900

    desgin : modal css수정

--- src/components/molecules/ModalBalloonList.jsx
+++ src/components/molecules/ModalBalloonList.jsx
@@ -9,13 +9,14 @@ export default function ModalBalloonList({ show, handleClose, balloons, onballoo
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>목록 보기</Modal.Title>
+        <Modal.Title>
+          <div className="container d-flex justify-content-start align-items-center gap-2">
+            <h5 className="m-0">현재 발견된 오물풍선 목록</h5>
+            <Badge bg="danger">{balloons.length}</Badge>
+          </div>
+        </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <div className="container d-flex justify-content-start align-items-center my-3 gap-2">
-          <h5 className="m-0">현재 발견된 오물풍선 목록</h5>
-          <Badge bg="danger">{balloons.length}</Badge>
-        </div>
         <div className="overflow-scroll">
           {balloons.map((balloon) => (
             <div key={balloon.id} onClick={() => onballoonClick(balloon.latitude, balloon.longitude)} style={{ cursor: 'pointer' }}>
