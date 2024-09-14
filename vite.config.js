ce747bc3f53eee9524f2cfa3ebe219ccf5836f6d
YANGSEOKWOO <didtjrdn030@naver.com>
2024-09-14 14:44:42 +0900

    feat : PWA 적용

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,56 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { createHtmlPlugin } from 'vite-plugin-html'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
-export default defineConfig (({mode}) => {
-  
-  const env = loadEnv(mode, process.cwd());
-  return{
-    plugins: [react(),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+        manifest: {
+          name: 'Vite PWA Project',
+          short_name: 'Vite PWA Project',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-64x64.png',
+              sizes: '64x64',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any',
+            },
+            {
+              src: 'maskable-icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable',
+            },
+          ],
+        },
+      }),
+
       createHtmlPlugin({
-        minify:true,
-        inject:{
-          data:{
+        minify: true,
+        inject: {
+          data: {
             kakaoApiKey: env.VITE_KAKAO_API_KEY,
-          }
-        }
-      })
+          },
+        },
+      }),
     ],
   }
-
-});
-
+})
