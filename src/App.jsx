41b77836c1a7ac169666ef216097d27a8ed9f229
YANGSEOKWOO <didtjrdn030@naver.com>
2024-09-14 17:10:22 +0900

    chore : router-dom 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ResponsiveLayout from './components/Layout/ResponsiveLayout'
 import { ErrorBoundary } from 'react-error-boundary'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   // TODO :: center위도 경도, 사용자의 위치를 가져와서 처음 위치를 볼 수 있도록 변환
@@ -11,11 +10,7 @@ function App() {
 
   return (
     <ErrorBoundary fallback={<div>Something went wrong</div>}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ResponsiveLayout />} />
-        </Routes>
-      </BrowserRouter>
+      <ResponsiveLayout />
     </ErrorBoundary>
   )
 }
