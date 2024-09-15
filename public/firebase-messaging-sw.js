3b4b9d0735e7f120135b1593416c7bafc052ce65
YANGSEOKWOO <didtjrdn030@naver.com>
2024-09-16 02:28:15 +0900

    chore:ds

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,31 +1,35 @@
-// importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-// importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
 
-// // Initialize the Firebase app in the service worker by passing in
-// // your app's Firebase config object.
-// // https://firebase.google.com/docs/web/setup#config-object
-// const firebaseApp = initializeApp({
-//   apiKey: 'AIzaSyDGtaTT8T0cE9fYpGjL6AaIjaH8qwd0sRQ',
-//   authDomain: 'balloon-map-net.firebaseapp.com',
-//   projectId: 'balloon-map-net',
-//   storageBucket: 'balloon-map-net.appspot.com',
-//   messagingSenderId: '643859630215',
-//   appId: '1:643859630215:web:45a110bbaed86bc1211022',
-//   measurementId: 'G-3HHX0FPMJR',
-// })
-// console.log('Firebase initialized message-sw:', firebaseApp)
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+const firebaseApp = initializeApp({
+  apiKey: 'AIzaSyDGtaTT8T0cE9fYpGjL6AaIjaH8qwd0sRQ',
+  authDomain: 'balloon-map-net.firebaseapp.com',
+  projectId: 'balloon-map-net',
+  storageBucket: 'balloon-map-net.appspot.com',
+  messagingSenderId: '643859630215',
+  appId: '1:643859630215:web:45a110bbaed86bc1211022',
+  measurementId: 'G-3HHX0FPMJR',
+})
+console.log('Firebase initialized message-sw:', firebaseApp)
 
-// // Retrieve an instance of Firebase Messaging so that it can handle background
-// // messages.
-// const messaging = firebaseApp.getMessaging(firebaseApp)
-// onBackgroundMessage(messaging, (payload) => {
-//   console.log('[firebase-messaging-sw.js] Received background message ', payload)
-//   // Customize notification here
-//   const notificationTitle = 'Background Message Title'
-//   const notificationOptions = {
-//     body: 'Background Message body.',
-//     icon: '/firebase-logo.png',
-//   }
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebaseApp.getMessaging(firebaseApp)
 
-//   self.registration.showNotification(notificationTitle, notificationOptions)
-// })
+messaging.onMessage((payload) => {
+  console.log('Message received:', payload)
+})
+messaging.onBackgroundMessage(messaging, (payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload)
+  // Customize notification here
+  const notificationTitle = 'Background Message Title'
+  //   const notificationOptions = {
+  //     body: 'Background Message body.',
+  //     icon: '/firebase-logo.png',
+  //   }
+
+  self.registration.showNotification(notificationTitle)
+})
