3e1fc19fc23ec566ca8d518a19606508ec2bad23
YANGSEOKWOO <didtjrdn030@naver.com>
2024-09-16 01:21:26 +0900

    chore: file 통일

--- src/components/atoms/RequestPermission.jsx
+++ src/components/atoms/RequestPermission.jsx
@@ -1,10 +1,29 @@
-import { getMessaging, getToken } from 'firebase/messaging'
-import { messaging } from '../../firebase'
+import { initializeApp } from 'firebase/app'
+import { getMessaging } from 'firebase/messaging'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDGtaTT8T0cE9fYpGjL6AaIjaH8qwd0sRQ',
+  authDomain: 'balloon-map-net.firebaseapp.com',
+  projectId: 'balloon-map-net',
+  storageBucket: 'balloon-map-net.appspot.com',
+  messagingSenderId: '643859630215',
+  appId: '1:643859630215:web:45a110bbaed86bc1211022',
+  measurementId: 'G-3HHX0FPMJR',
+}
 
 // Get registration token. Initially this makes a network call, once retrieved
 // subsequent calls to getToken will return from cache.
 
 export default function RequestPermission() {
+  // Initialize Firebase
+  // Firebase 초기화
+  const app = initializeApp(firebaseConfig)
+  const messaging = getMessaging(app)
+
   console.log('Requesting permission...')
   Notification.requestPermission().then((permission) => {
     if (permission === 'granted') {
--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
-import { getMessaging } from 'firebase/messaging'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from 'firebase/app'
+// import { getMessaging } from 'firebase/messaging'
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyDGtaTT8T0cE9fYpGjL6AaIjaH8qwd0sRQ',
-  authDomain: 'balloon-map-net.firebaseapp.com',
-  projectId: 'balloon-map-net',
-  storageBucket: 'balloon-map-net.appspot.com',
-  messagingSenderId: '643859630215',
-  appId: '1:643859630215:web:45a110bbaed86bc1211022',
-  measurementId: 'G-3HHX0FPMJR',
-}
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDGtaTT8T0cE9fYpGjL6AaIjaH8qwd0sRQ',
+//   authDomain: 'balloon-map-net.firebaseapp.com',
+//   projectId: 'balloon-map-net',
+//   storageBucket: 'balloon-map-net.appspot.com',
+//   messagingSenderId: '643859630215',
+//   appId: '1:643859630215:web:45a110bbaed86bc1211022',
+//   measurementId: 'G-3HHX0FPMJR',
+// }
 
-// Initialize Firebase
-// Firebase 초기화
-const app = initializeApp(firebaseConfig)
-const messaging = getMessaging(app)
+// // Initialize Firebase
+// // Firebase 초기화
+// const app = initializeApp(firebaseConfig)
+// const messaging = getMessaging(app)
 
 export { messaging }
