4f7472c8625c7893cf9aa3de26f24479feee36ff
YANGSEOKWOO <didtjrdn030@naver.com>
2024-09-16 01:09:55 +0900

    chore:token 설정

---
+++ src/components/atoms/Notification.js
@@ -0,0 +1,30 @@
+import { getMessaging, getToken } from 'firebase/messaging'
+
+// Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+
+export default function requestPermission() {
+  console.log('Requesting permission...')
+  Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+      console.log('Notification permission granted.')
+      const messaging = getMessaging()
+      getToken(messaging, { vapidKey: 'BL6oABzSftdRmaeSIMegPzCdeEWOPk7D5DPGa8WhKOZWbYmsgem26BWX1gqIWbvsQ64XfIuMSNyYlKXQoK8hfaQ' })
+        .then((currentToken) => {
+          if (currentToken) {
+            console.log('currentToken:', currentToken)
+            // Send the token to your server and update the UI if necessary
+            // ...
+          } else {
+            // Show permission request UI
+            console.log('No registration token available. Request permission to generate one.')
+            // ...
+          }
+        })
+        .catch((err) => {
+          console.log('An error occurred while retrieving token. ', err)
+          // ...
+        })
+    }
+  })
+}
