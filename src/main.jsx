284eac189231cd0a4d2cd77082f606199dbcff58
YANGSEOKWOO <didtjrdn030@naver.com>
2024-09-16 00:16:51 +0900

    fix: serviceWorker 로직수정

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+// Service Worker 등록 코드 추가
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/firebase-messaging-sw.js') // public 폴더에 있는 Service Worker 등록
+      .then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope)
+      })
+      .catch((err) => {
+        console.log('ServiceWorker registration failed: ', err)
+      })
+  })
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
