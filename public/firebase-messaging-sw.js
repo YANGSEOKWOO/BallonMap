de7f634ebeb354892470819982e6b66d43977c9d
YANGSEOKWOO <didtjrdn030@naver.com>
2024-09-16 02:32:10 +0900

    chore:nn

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,7 +4,7 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-firebaseApp.initializeApp({
+firebase.initializeApp({
   apiKey: 'AIzaSyDGtaTT8T0cE9fYpGjL6AaIjaH8qwd0sRQ',
   authDomain: 'balloon-map-net.firebaseapp.com',
   projectId: 'balloon-map-net',
@@ -13,11 +13,11 @@ firebaseApp.initializeApp({
   appId: '1:643859630215:web:45a110bbaed86bc1211022',
   measurementId: 'G-3HHX0FPMJR',
 })
-console.log('Firebase initialized message-sw:', firebaseApp)
+console.log('Firebase initialized message-sw:', firebase)
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = firebaseApp.getMessaging(firebaseApp)
+const messaging = firebase.messaging()
 
 messaging.onMessage((payload) => {
   console.log('Message received:', payload)
