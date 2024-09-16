198bed668df846d86e81aeec966371267724b2d3
YANGSEOKWOO <didtjrdn030@naver.com>
2024-09-16 19:48:36 +0900

    feat: Bg, fg이벤트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ResponsiveLayout from './components/Layout/ResponsiveLayout'
 import { ErrorBoundary } from 'react-error-boundary'
 import { LocationProvider } from './context/LocationContext'
 import RequestPermission from './components/atoms/RequestPermission'
 
 function App() {
+  useEffect(() => {
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === 'visible') {
+        console.log('App is in the foreground')
+        // 포그라운드로 전환될 때 실행할 로직
+      } else {
+        console.log('App is in the background')
+        // 백그라운드로 전환될 때 실행할 로직
+      }
+    }
+
+    document.addEventListener('visibilitychange', handleVisibilityChange)
+
+    // 클린업 함수로 이벤트 리스너 제거
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange)
+    }
+  }, [])
   return (
     <ErrorBoundary FallbackComponent={() => <div>Error</div>}>
       <LocationProvider>
